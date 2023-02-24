cf6c17aff26bafd64af11c31b46b5bba23da2d79
Jieun Kim <jieunkim1379@gmail.com>
2023-02-24 10:31:16 -0500

    logIn logOut button ternary

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { logOut } from "../HelperFunctions";
-import { Link } from "react-router-dom";
+import { isLoggedIn, logOut } from "../HelperFunctions";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate()
   return (
     <div id="navbar">
       <nav>
       <Link className="navButton" to="/">Post</Link>
       <Link className="navButton" to='profile'>Profile</Link>
-      <Link className="navButton" to="login">Login</Link>
-      <button onClick={logOut} >Log Out</button>
+      {isLoggedIn() ? 
+      (<button onClick={logOut} >Log Out</button>,
+      // alert("You've logged out"),
+      navigate('/login')):
+      <Link className="navButton" to="login">Login</Link>}
       </nav>
     </div>
   );
 };
+
+console.log(isLoggedIn())
+
+
 
 export default Navbar;
